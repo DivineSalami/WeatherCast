c32e216ec498bae9b838bf4dbd0861e96aa7a1d8
p.m.student <Bolden.Chance02@gmail.com>
2021-04-26 12:31:25 -0500

    Initial Commit

---
+++ WeatherCast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherCast
+//
+//  Created by P.M. Student on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherCast/WeatherCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCastApp.swift
+//  WeatherCast
+//
+//  Created by P.M. Student on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
