987b19abe18c102599a5eabd02beb871f5f90374
p.m.student <Bolden.Chance02@gmail.com>
2021-04-29 13:20:46 -0500

    more added

--- WeatherCast/Models/DailyWeather.swift
+++ WeatherCast/Models/DailyWeather.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct DailyWeather: Codable, Identifiable {
+    var dt: Int
+    var temperature: Temperature
+    var weather: [WeatherDetail]
+    
+    enum CodingKey: String {
+        case dt
+        case temperature
+        case weather
+    }
+    init() {
+         dt = 0
+        temperature = Temperature(min: 0.0, max: 0.0)
+        weather = [WeatherDetail(main: "", description: "", icon: "")]
+    }
+}
+
+extension DailyWeather {
+    var id: UUID {
+        return UUID()
+    }
+}
